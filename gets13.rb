a1bd10d79440b8b4483f03b497bc11f6b1f10f09
mohamedmastour <mmastour7@gmail.com>
2023-09-14 12:32:22 +0800

    add code

--- gets13.rb
+++ gets13.rb
@@ -1,15 +1,17 @@
-# gets 
+gets 
 
-# puts "Please Enter your Name :"
-# name = gets
-# puts ("your Name is  : " + name)
+puts "Please Enter your Name :"
+name = gets
+puts ("your Name is  : " + name)
 
 
 
-# puts "Please Enter your Web site :"
-# site = gets
-# puts ("your web site is  : " + site)
+puts "Please Enter your Web site :"
+site = gets
+puts ("your web site is  : " + site)
 
-# print "Enter your age : "
-# age = gets.chomp.to_i
-# print "your age is :  #{age}"
+print "Enter your age : "
+age = gets.chomp.to_i
+print "your age is :  #{age}"
+
+puts age
